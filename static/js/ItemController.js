db43da03bdd45508227fdd03b07b5ca7fbb977fe
Kaytee Cannon <kaytee.cannon@me.com>
2020-11-17 02:25:59 +0000

    Adds ability to click each item to edit

--- static/js/ItemController.js
+++ static/js/ItemController.js
@@ -1,7 +1,18 @@
 "use strict"
 
-function openItemEditor() {
-    window.location.href = "http://localhost:5000/user/1/inventory/item-editor/1"
+function makeItemsClickable(response) {
+
+    const itemIDs = [];
+    for (const [key, value] of Object.entries(response)) {
+        itemIDs.push(value);
+      }
+
+    for (const id of itemIDs) {
+        $(`#item-${id}-row`).on("click", () => {
+            window.location.href = `http://localhost:5000/user/1/inventory/item-editor/${id}`;
+        });
+    }
+    
 }
 
-$("#item-1-row").on("click", openItemEditor);
+$.get('/get-item-ids', makeItemsClickable);
