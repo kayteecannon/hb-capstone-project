4dee0bd2acaadadfe2d35097dd494e1f26eac239
Kaytee Cannon <kaytee.cannon@me.com>
2020-11-25 15:31:28 +0000

    Adds test_create_inventory unit test

--- test_crud.py
+++ test_crud.py
@@ -4,7 +4,6 @@ import doctest
 import os
 from model import db, User, Inventory, Item, connect_to_db
 
-
 os.system('dropdb capstonetest')
 os.system('createdb capstonetest')
 
@@ -14,6 +13,12 @@ class MyAppUnitTestCase(unittest.TestCase):
         """Test create_user function by checking if instance returned is User."""
         user = crud.create_user(email='test1@test.com', password='test')
         self.assertIsInstance(user, User)
+
+    def test_create_inventory(self):
+        """Test create_inventory function by checking if instance returned is Inventory."""
+        user = crud.create_user(email='test2@test.com', password='test')
+        inventory = crud.create_inventory(user_id=user.user_id, title='test inventory')
+        self.assertIsInstance(inventory, Inventory)
 
 if __name__ == '__main__':
     from server import app
